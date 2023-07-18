0b04b6a472379447903c6081e58c73c3f2a9999b
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-07-18 11:12:23 -0500

    first commit

---
+++ src/test/java/com/proyecto/faan/FaanBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.faan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaanBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
