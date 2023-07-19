f62d813bc869c6e36a3733468a4bdfde0d477b3a
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-07-19 11:07:54 -0500

    new method generic, find by name any atribute and another changes
    new method generic, find by name any atribute and another changes

--- src/main/java/com/proyecto/faan/controller/Generic/GenericController.java
+++ src/main/java/com/proyecto/faan/controller/Generic/GenericController.java
@@ -1,11 +1,13 @@
 package com.proyecto.faan.controller.Generic;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.Serializable;
 import java.util.List;
@@ -13,6 +15,7 @@ import java.util.List;
 public interface GenericController <T, ID extends Serializable> {
     public ResponseEntity<List<T>> findByAll();
     public ResponseEntity<?> findByAll(@PageableDefault(page = 0, size = 3, direction = Sort.Direction.ASC) Pageable pageable);
+    ResponseEntity<Page<T>> findByAll(@PageableDefault(page = 0, size = 3, direction = Sort.Direction.ASC) Pageable pageable, @RequestParam String columnName,  @RequestParam String value);
 
     public ResponseEntity<T> findByOne(@PathVariable ID id);
     public ResponseEntity<?> save(@RequestBody T entity);
--- src/main/java/com/proyecto/faan/controller/Generic/GenericControllerImpl.java
+++ src/main/java/com/proyecto/faan/controller/Generic/GenericControllerImpl.java
@@ -2,6 +2,7 @@ package com.proyecto.faan.controller.Generic;
 
 import com.proyecto.faan.service.generic.GenericService;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,6 +30,16 @@ public abstract class GenericControllerImpl <T, ID extends Serializable> impleme
             return new ResponseEntity<>(getService().findByAll(pageable), HttpStatus.OK);
         }catch (Exception e){
             System.out.println(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Override
+    @GetMapping("/pageable/find")
+    public ResponseEntity<Page<T>> findByAll(Pageable pageable, String columnName, String value) {
+        try {
+            return new ResponseEntity<>(getService().findByAll(pageable, columnName, value), HttpStatus.OK);
+        }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -71,6 +82,8 @@ public abstract class GenericControllerImpl <T, ID extends Serializable> impleme
             }
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
+        }catch (DataIntegrityViolationException e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }catch (Exception e){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
