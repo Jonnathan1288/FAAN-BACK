b19adcd5950648933b00154d482cc29d79583e7e
WI1L0 <william.martinez.est@tecazuay.edu.ec>
2023-08-22 00:22:50 -0500

    ..
    fg

--- src/main/java/com/proyecto/faan/service/mongoService/NotificacionService.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.faan.service.mongoService;
-
-
-import com.proyecto.faan.model.secundary.Notificacion;
-import com.proyecto.faan.service.generic.GenericService;
-
-public interface NotificacionService{
-    public Notificacion save(Notificacion notificacion);
-}
