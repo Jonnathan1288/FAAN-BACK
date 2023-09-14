e2c37a3c0fefea4615d6ffa1aecb0fc4d4f03aaf
Jonnathan <91436348+Jonnathan1288@users.noreply.github.com>
2023-09-14 02:17:05 -0500

    other
    other

--- src/main/java/com/proyecto/faan/service/primarys/EstadoAnimalService.java
+++ src/main/java/com/proyecto/faan/service/primarys/EstadoAnimalService.java
@@ -1,7 +1,12 @@
 package com.proyecto.faan.service.primarys;
 
 import com.proyecto.faan.model.primarys.EstadoAnimal;
+import com.proyecto.faan.model.primarys.RazaAnimal;
 import com.proyecto.faan.service.generic.GenericService;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EstadoAnimalService extends GenericService<EstadoAnimal, Integer> {
+
 }
--- src/main/java/com/proyecto/faan/service/primarys/TipoAnimalService.java
+++ src/main/java/com/proyecto/faan/service/primarys/TipoAnimalService.java
@@ -3,9 +3,13 @@ package com.proyecto.faan.service.primarys;
 import com.proyecto.faan.model.primarys.TipoAnimal;
 import com.proyecto.faan.payload.PeyloadNumeroAnimalTipo;
 import com.proyecto.faan.service.generic.GenericService;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface TipoAnimalService extends GenericService<TipoAnimal, Integer> {
     List<PeyloadNumeroAnimalTipo> countAnimalesByTipoAnimal();
+
+    public List<TipoAnimal> findByEstadoTipo();
+
 }
