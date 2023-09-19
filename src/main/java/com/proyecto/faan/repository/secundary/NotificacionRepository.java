00fd971176e09ae6f7c397817947173c4e0ce987
EstebanBacuilima <davidbacuilima13@gmail.com>
2023-09-18 20:32:30 -0500

    notifcation method

--- src/main/java/com/proyecto/faan/repository/secundary/NotificacionRepository.java
+++ src/main/java/com/proyecto/faan/repository/secundary/NotificacionRepository.java
@@ -1,13 +1,18 @@
 package com.proyecto.faan.repository.secundary;
 
 import com.proyecto.faan.model.secundary.Notificacion;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface NotificacionRepository extends MongoRepository<Notificacion, String> {
 
     public List<Notificacion> findByEstadoNotifacion(String estado);
+
+    @Query("{'estadoNotifacion': 'A', 'proximaFechaFacunacion': {$gte: ?0, $lte: ?1}}")
+    public List<Notificacion> findNotificacionesProximaVacuna(Date fechaActual, Date fechaFutura);
 }
